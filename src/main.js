1bc9dd1f7370de6749505da06bff6f3687bcffcc
Sunny <me@darkowlzz.space>
2016-03-01 13:12:16 +0530

    app quit on windows close

--- src/main.js
+++ src/main.js
@@ -9,9 +9,24 @@ let mainWindow;
 function createWindow () {
   mainWindow = new BrowserWindow({width: 400, height: 400});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', () => {
     mainWindow = null;
   });
 }
 
 app.on('ready', createWindow);
+
+// Quit app on all windows close
+app.on('window-all-closed', () => {
+  // not for OS X
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// Recreate a window when the app is activated on OS X
+app.on('activate', () => {
+  if (mainWindow == null) {
+    createWindow();
+  }
+});
